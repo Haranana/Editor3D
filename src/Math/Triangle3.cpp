24f4896d84bb8ffceb0ddf34f1287862f040d1d4
Haranana <hubert.osmalek@op.pl>
2025-07-31 13:30:31 +0200

    quick fix to Triangle3 class

--- src/Math/Triangle3.cpp
+++ src/Math/Triangle3.cpp
@@ -43,14 +43,14 @@ std::pair<int, int> Triangle3::getBorderX(int lowest, int highest){
 }
 
 std::pair<int, int> Triangle3::getBorderY(int lowest, int highest){
-    int minY = std::max(lowest, static_cast<int>(std::floor(std::min({v1.x, v2.x, v3.x}))));
-    int maxY = std::min(highest - 1, static_cast<int>(std::ceil(std::max({v1.x, v2.x, v3.x}))));
+    int minY = std::max(lowest, static_cast<int>(std::floor(std::min({v1.y, v2.y, v3.y}))));
+    int maxY = std::min(highest - 1, static_cast<int>(std::ceil(std::max({v1.y, v2.y, v3.y}))));
     return {minY, maxY};
 }
 
 std::pair<int, int> Triangle3::getBorderZ(int lowest, int highest){
-    int minZ = std::max(lowest, static_cast<int>(std::floor(std::min({v1.x, v2.x, v3.x}))));
-    int maxZ = std::min(highest - 1, static_cast<int>(std::ceil(std::max({v1.x, v2.x, v3.x}))));
+    int minZ = std::max(lowest, static_cast<int>(std::floor(std::min({v1.z, v2.z, v3.z}))));
+    int maxZ = std::min(highest - 1, static_cast<int>(std::ceil(std::max({v1.z, v2.z, v3.z}))));
     return {minZ, maxZ};
 }
 
